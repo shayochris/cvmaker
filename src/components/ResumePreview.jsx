3a39f18c23c94babff4885fa785d8cb8495ad2cf
chris <chrisshayo4@gmail.com>
2024-03-17 03:33:25 +0300

    react-pdf

--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -1,165 +1,199 @@
 import React from 'react'
-import { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+import avatar from '../images/avatar.jpg'
+import { Page, Text, Image, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
+
+//icons
+import { IoMdMail } from "react-icons/io";
+
 
 
 const ResumePreview = () => (
   <Document>
     <Page size="A4" style={styles.page}>
-      <View style={styles.body}>
-        <View style={styles.flex_c}>
-          <View style={{ textAlign: 'center', width: '100%', padding: 20 }}>
-            <Text style={styles.text_large}>CHRISTOPHER HONEST SHAYO</Text>
-            <Text style={{ color: '#ff6f3c', textTransform: 'uppercase' }}>
-              web developer
-            </Text>
+      <View style={[styles.body, styles.flex]}>
+        <View style={
+          {
+            width: '40%',
+            backgroundColor: '#002D62',
+            color: 'white',
+            height: '100%'
+          }}>
+          <View style={[{ padding: 20 }, styles.flexC]} >
+            <Image
+              source={avatar}
+              style={styles.avatar}
+            />
+          </View>
+          <View style={{ padding: 20 }}>
+            <Text style={{
+              fontSize: 20,
+              marginBottom: 12,
+              paddingVertical: 8,
+              marginBottom: 8,
+              borderBottom: 1,
+              borderBottomColor: 'white',
+            }}>Contact</Text>
+            <View style={{ marginBottom: 12 }}>
+              <Text>Phone</Text>
+              <Text>0766 495 628</Text>
+            </View>
+            <View style={{ marginBottom: 12 }}>
+              <Text>Email</Text>
+              <Text>chrisshayo4@gmail.com</Text>
+            </View>
+            <View style={{ marginBottom: 12 }}>
+              <Text>Address</Text>
+              <Text>Dar es Salaam</Text>
+            </View>
+          </View>
+          <View style={{ padding: 20 }}>
+            <Text style={{
+              fontSize: 20,
+              marginBottom: 12,
+              paddingVertical: 8,
+              marginBottom: 8,
+              borderBottom: 1,
+              borderBottomColor: 'white',
+            }}>Education</Text>
+            <View style={{ marginBottom: 12 }}>
+              <Text>Bachelor Degree</Text>
+              <Text>University of Dodoma</Text>
+              <Text>2018-2021</Text>
+            </View>
+            <View style={{ marginBottom: 12 }}>
+              <Text>Bachelor Degree</Text>
+              <Text>University of Dodoma</Text>
+              <Text>2018-2021</Text>
+            </View>
+            <View style={{ marginBottom: 12 }}>
+              <Text>Bachelor Degree</Text>
+              <Text>University of Dodoma</Text>
+              <Text>2018-2021</Text>
+            </View>
+          </View>
+          <View style={{ padding: 20 }}>
+            <Text style={{
+              fontSize: 20,
+              marginBottom: 8,
+              paddingVertical: 8,
+              marginBottom: 8,
+              borderBottom: 1,
+              borderBottomColor: 'white',
+            }}>Skills</Text>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+          </View>
+          <View style={{ padding: 20 }}>
+            <Text style={{
+              fontSize: 20,
+              paddingVertical: 8,
+              marginBottom: 8,
+              borderBottom: 1,
+              borderBottomColor: 'white',
+            }}>Languages</Text>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+            <View style={{ marginBottom: 8 }}>
+              <Text>Bachelor Degree</Text>
+            </View>
+
           </View>
         </View>
-        <View style={styles.flex}>
-          <View style={{ width: '40%', padding: 20, marginTop: 12 }}>
-            <View>
-              <View style={[styles.flex, { marginBottom: 8 }]}>
-                <Text style={styles.boldText}>Email</Text>
-                <Text style={{ marginLeft: 8 }}>chrisshayo4@gmail.com</Text>
-              </View>
-              <View style={[styles.flex, { marginBottom: 8 }]}>
-                <Text style={styles.boldText}>Phone</Text>
-                <Text style={{ marginLeft: 8 }}>0766495628</Text>
-              </View>
-              <View style={[styles.flex, { marginBottom: 8 }]}>
-                <Text style={styles.boldText}>Address</Text>
-                <Text style={{ marginLeft: 8 }}>Dar Es Salaam, Tanzania</Text>
-              </View>
-              <View style={[styles.flex, { marginBottom: 8 }]}>
-                <Text style={styles.boldText}>Email</Text>
-                <Text style={{ marginLeft: 8 }}>chrisshayo4@gmail.com</Text>
-              </View>
-            </View>
-            <View style={{ marginBottom: 14 }}>
-              <Text style={[styles.boldText, { marginVertical: 8, color: '#ff6f3c' }]}>EDUCATION</Text>
-              <View style={{ marginBottom: 8 }}>
-                <Text style={[styles.boldText, { paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  bachelor degree
-                </Text>
-                <Text style={[{ paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  university of Dodoma
-                </Text>
-                <Text style={[{ marginBottom: 6 }]}>march 2012 - june 2016</Text>
-              </View>
-              <View style={{ marginBottom: 8 }}>
-                <Text style={[styles.boldText, { paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  bachelor degree
-                </Text>
-                <Text style={[{ paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  university of Dodoma
-                </Text>
-                <Text style={[{ marginBottom: 6 }]}>march 2012 - june 2016</Text>
-              </View>
-              <View style={{ marginBottom: 8 }}>
-                <Text style={[styles.boldText, { paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  bachelor degree
-                </Text>
-                <Text style={[{ paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  university of Dodoma
-                </Text>
-                <Text style={[{ marginBottom: 6 }]}>march 2012 - june 2016</Text>
-              </View>
+        <View style={{ padding: 16, width: '60%', color: '#6b7280' }}>
+          <Text style={{ fontSize: 24, color: '#002D62' }}>Christopher Honest Shayo</Text>
+          <Text style={{ fontSize: 12, marginBottom: 12 }}>Software Developer</Text>
+          <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui iure, labore quae deserunt dolores deleniti facere cupiditate sapiente quasi similique recusandae veniam consequuntur, consectetur nulla neque commodi corrupti in pariatur?
+          </Text>
 
-            </View>
-
-          </View>
-
-          <View style={{ width: '60%', padding: 20 }}>
-            <View style={{ marginBottom: 12 }}>
-              <Text style={[styles.boldText, { paddingBottom: 4, color: '#ff6f3c' }]}>
-                INTRODUCTION
+          <View style={{ marginTop: 20 }}>
+            <Text style={{
+              fontSize: 20,
+              color: '#002D62',
+              borderBottom: 1,
+              borderBottomColor: '#002D62',
+              paddingVertical: 4,
+            }}>Experience</Text>
+            <View style={{ marginTop: 8 }}>
+              <Text style={{ color: '#ff6f3c' }} >
+                Software Developer | GPITG LTD
               </Text>
+              <Text style={{ marginVertical: 4 }}>2021-2024</Text>
               <Text>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet autem possimus magni aliquid excepturi rerum vel facere corporis, quasi dolor nostrum id. Ad voluptas suscipit distinctio praesentium nisi qui recusandae?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus, debitis dolorem ratione perspiciatis suscipit magnam rerum laborum voluptate nulla odio, quae voluptates, minima quis saepe consequuntur. Facilis, magnam fuga?
               </Text>
             </View>
-
-            <View style={{ marginBottom: 14 }}>
-              <Text style={[styles.boldText, { paddingBottom: 4, color: '#ff6f3c' }]}>EXPERIENCE</Text>
-              <View style={{ marginBottom: 8 }}>
-                <Text style={[styles.boldText, { paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  software developer | GPITG LTD
-                </Text>
-                <Text style={[styles.boldText, { marginBottom: 6 }]}>march 2012 - june 2016</Text>
-                <Text>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet autem possimus magni aliquid excepturi rerum vel facere corporis, quasi dolor nostrum id. Ad voluptas suscipit distinctio praesentium nisi qui recusandae?
-                </Text>
-              </View>
-              <View style={{ marginBottom: 8 }}>
-                <Text style={[styles.boldText, { paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  software developer | GPITG LTD
-                </Text>
-                <Text style={[styles.boldText, { marginBottom: 6 }]}>march 2012 - june 2016</Text>
-                <Text>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet autem possimus magni aliquid excepturi rerum vel facere corporis, quasi dolor nostrum id. Ad voluptas suscipit distinctio praesentium nisi qui recusandae?
-                </Text>
-              </View>
-              <View style={{ marginBottom: 8 }}>
-                <Text style={[styles.boldText, { paddingBottom: 4, textTransform: 'capitalize' }]}>
-                  software developer | GPITG LTD
-                </Text>
-                <Text style={[styles.boldText, { marginBottom: 6 }]}>march 2012 - june 2016</Text>
-                <Text>
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet autem possimus magni aliquid excepturi rerum vel facere corporis, quasi dolor nostrum id. Ad voluptas suscipit distinctio praesentium nisi qui recusandae?
-                </Text>
-              </View>
-            </View>
-
-
-            <View style={{ marginBottom: 12 }}>
-              <Text style={[styles.boldText, { paddingBottom: 4, color: '#ff6f3c' }]}>
-                SKILLS
+            <View style={{ marginTop: 8 }}>
+              <Text style={{ color: '#ff6f3c' }} >
+                Software Developer | GPITG LTD
               </Text>
-              <View style={styles.flex_b}>
-                <View>
-                  <Text>James john</Text>
-                  <Text>Lecturer</Text>
-                  <Text>Udom</Text>
-                </View>
-                <View>
-                  <Text>James john</Text>
-                  <Text>Lecturer</Text>
-                  <Text>Udom</Text>
-                </View>
-                <View>
-                  <Text>James john</Text>
-                  <Text>Lecturer</Text>
-                  <Text>Udom</Text>
-                </View>
-
-
-              </View>
-            </View>
-            <View style={{ marginBottom: 12 }}>
-              <Text style={[styles.boldText, { paddingBottom: 4, color: '#ff6f3c' }]}>
-                REFEREES
+              <Text style={{ marginVertical: 4 }}>2021-2024</Text>
+              <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus, debitis dolorem ratione perspiciatis suscipit magnam rerum laborum voluptate nulla odio, quae voluptates, minima quis saepe consequuntur. Facilis, magnam fuga?
               </Text>
-              <View style={styles.flex_b}>
-                <View>
-                  <Text>James john</Text>
-                  <Text>Lecturer</Text>
-                  <Text>Udom</Text>
-                </View>
-                <View>
-                  <Text>James john</Text>
-                  <Text>Lecturer</Text>
-                  <Text>Udom</Text>
-                </View>
-                <View>
-                  <Text>James john</Text>
-                  <Text>Lecturer</Text>
-                  <Text>Udom</Text>
-                </View>
-
-              </View>
+            </View>
+            <View style={{ marginTop: 8 }}>
+              <Text style={{ color: '#ff6f3c' }} >
+                Software Developer | GPITG LTD
+              </Text>
+              <Text style={{ marginVertical: 4 }}>2021-2024</Text>
+              <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non doloribus, debitis dolorem ratione perspiciatis suscipit magnam rerum laborum voluptate nulla odio, quae voluptates, minima quis saepe consequuntur. Facilis, magnam fuga?
+              </Text>
             </View>
           </View>
-        </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={{
+              fontSize: 20,
+              color: '#002D62',
+              borderBottom: 1,
+              borderBottomColor: '#002D62',
+              paddingVertical: 4,
+            }}>Reference</Text>
+            <View style={{ marginTop: 8 }}>
+              <Text style={{ color: '#ff6f3c' }} >
+                John Doe
+              </Text>
+              <Text>Lecturer | University of Dodoma</Text>
+              <Text>email@gmail.com</Text>
+              <Text>0766 495 628</Text>
+            </View>
+            <View style={{ marginTop: 8 }}>
+              <Text style={{ color: '#ff6f3c' }} >
+                John Doe
+              </Text>
+              <Text>Lecturer | University of Dodoma</Text>
+              <Text>email@gmail.com</Text>
+              <Text>0766 495 628</Text>
+            </View>
+            <View style={{ marginTop: 8 }}>
+              <Text style={{ color: '#ff6f3c' }} >
+                John Doe
+              </Text>
+              <Text>Lecturer | University of Dodoma</Text>
+              <Text>email@gmail.com</Text>
+              <Text>0766 495 628</Text>
+            </View>
+          </View>
 
+        </View>
       </View>
     </Page>
   </Document>
@@ -169,34 +203,41 @@ export default ResumePreview
 
 const styles = StyleSheet.create({
   body: {
-    padding: 20,
-    fontSize: 12,
-    color: '#64748b'
-  },
-  flex_c: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  flex_b: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
+    width: '100%',
+    height: '100%',
+    fontSize: 12
   },
   flex: {
     flexDirection: 'row',
   },
-  text_large: {
-    textTransform: 'uppercase',
-    fontSize: 20,
-    color: '#002D62'
+  flexA: {
+    flexDirection: 'row',
+    alignItems: 'center'
   },
-  boldText: {
-    color: '#020617'
+  flexB: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  text_uppercase: {
-    textTransform: 'uppercase'
+  flexC: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: '100%'
+  },
+  icon: {
+    width: 20,
+    height: 20
+  },
+  tab: {
+    backgroundColor: '#6b7280',
+    color: 'white',
+    padding: 2,
+    width: '100%'
   }
-
 });
 
